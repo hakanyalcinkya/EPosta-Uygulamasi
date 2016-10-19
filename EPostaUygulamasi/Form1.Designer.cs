0263000f5398f462b2db166666937b70c7fd444f
Hakan yalçınkaya <hknnujen47@outlook.com>
2016-10-19 18:02:22 +0300

    Add project files.

---
+++ EPostaUygulamasi/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace EPostaUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKonu = new System.Windows.Forms.TextBox();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.cmbEPosta = new System.Windows.Forms.ComboBox();
+            this.btnHesapAc = new System.Windows.Forms.LinkLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblDurum = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EPosta :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Konu :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mesaj :";
+            // 
+            // txtKonu
+            // 
+            this.txtKonu.Location = new System.Drawing.Point(87, 63);
+            this.txtKonu.Name = "txtKonu";
+            this.txtKonu.Size = new System.Drawing.Size(121, 20);
+            this.txtKonu.TabIndex = 3;
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Location = new System.Drawing.Point(87, 102);
+            this.txtMesaj.Multiline = true;
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(237, 138);
+            this.txtMesaj.TabIndex = 4;
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(239, 35);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(85, 33);
+            this.btnGonder.TabIndex = 5;
+            this.btnGonder.Text = "Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // cmbEPosta
+            // 
+            this.cmbEPosta.FormattingEnabled = true;
+            this.cmbEPosta.Location = new System.Drawing.Point(87, 35);
+            this.cmbEPosta.Name = "cmbEPosta";
+            this.cmbEPosta.Size = new System.Drawing.Size(121, 21);
+            this.cmbEPosta.TabIndex = 6;
+            // 
+            // btnHesapAc
+            // 
+            this.btnHesapAc.AutoSize = true;
+            this.btnHesapAc.Location = new System.Drawing.Point(12, 9);
+            this.btnHesapAc.Name = "btnHesapAc";
+            this.btnHesapAc.Size = new System.Drawing.Size(54, 13);
+            this.btnHesapAc.TabIndex = 7;
+            this.btnHesapAc.TabStop = true;
+            this.btnHesapAc.Text = "Hesap Aç";
+            this.btnHesapAc.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblDurum});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(352, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(118, 17);
+            this.lblDurum.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 261);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnHesapAc);
+            this.Controls.Add(this.cmbEPosta);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.txtMesaj);
+            this.Controls.Add(this.txtKonu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKonu;
+        private System.Windows.Forms.TextBox txtMesaj;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.ComboBox cmbEPosta;
+        private System.Windows.Forms.LinkLabel btnHesapAc;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDurum;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EPostaUygulamasi/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EPostaUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        string eposta = string.Empty;
+        string sifre = string.Empty;
+        string sunucu = string.Empty;
+        int port = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.ShowDialog();
+            eposta = frm.txtEpostaHesap.Text.Trim();
+            sifre = frm.txtSifre.Text.Trim();
+            sunucu = frm.txtSunucu.Text.Trim();
+            port = (int)frm.txtPort.Value;
+            if (eposta != "" && sifre != "" && sunucu != "")
+            {
+                btnGonder.Enabled = true;
+                txtKonu.Enabled = true;
+                txtMesaj.Enabled = true;
+                cmbEPosta.Enabled = true;
+
+                lblDurum.Text = "Hazır (" + eposta + " adresi ile hesap açıldı.)";
+
+                btnHesapAc.Text = eposta;
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli bir hesap açınız.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnGonder.Enabled = false;
+            txtKonu.Enabled = false;
+            txtMesaj.Enabled = false;
+            cmbEPosta.Enabled = false;
+            lblDurum.Text = "Lütfen bir hesap açınız.";
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            NetworkCredential kimlik = new NetworkCredential();
+            kimlik.UserName = eposta;
+            kimlik.Password = sifre;
+
+            SmtpClient client = new SmtpClient();
+            client.Host = sunucu;
+            client.Port = port;
+
+            client.Credentials = kimlik;
+            client.EnableSsl = true;
+
+            MailMessage posta = new MailMessage();
+            posta.IsBodyHtml = true;
+            posta.Subject = txtKonu.Text;
+            posta.Body = txtMesaj.Text;
+            try
+            {
+                posta.From = new MailAddress(eposta);
+                posta.To.Add(new MailAddress(cmbEPosta.Text));
+
+                // Sunucuya mesajı gönder komutu verilir.
+                // http://www.yazilimindunyasi.com/c-windows-form-application/63-gmail-uzerinden-mail-gonderme-programi.html
+                client.Send(posta);
+
+                lblDurum.Text = "Mail gönderildi";
+                lblDurum.ForeColor = Color.Green;
+
+                MailAdresiKaydet(cmbEPosta.Text);
+
+                timer1.Enabled = true;
+                //timer1.Start();
+            }
+            catch (Exception ex)
+            {
+                lblDurum.Text = "Hata oluştu.";
+                lblDurum.ForeColor = Color.Red;
+
+                MessageBox.Show(ex.Message, "Hata Mesajı");
+                MessageBox.Show(ex.Source, "Hata Kaynağı");
+                MessageBox.Show(ex.StackTrace, "Hata Detay");
+            }
+        }
+    }
+}
